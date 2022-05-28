0f2e4da18b566d9a6d5caa2f8945c7bbadac3b04
Jesse Y Lin <jesseyounglin@gmail.com>
2022-05-28 01:18:21 -0400

    Fixed transposition bug in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,16 +6,17 @@ a = [1 2;
 b = [5 6;
      7 8]
 c = [5 10; 15 20;;; 7 14; 21 28;;;; 6 12; 18 24;;; 8 16; 24 32]
-@testset "Kronecker" begin
+@testset "Kronecker functional test" begin
     @test a ⊗ b == c
 end
 
-num_offices = 3
-num_candidates = 4
-prefvecs = [rand() for i in 1:num_offices, j in 1:num_candidates]
-@testset "VotePreferences" begin
+const num_offices = 3
+const num_candidates = 5
+const num_voters = 6
+@testset "VotePreferences sanity check" begin
+    prefvecs = [rand() for i in 1:num_candidates, j in 1:num_offices]
     vp = VotePreference(prefvecs)
     @test typeof(vp) == VotePreference
-    @test mapslices(norm, vp.prefvecs; dims=1) ≈ reshape([1. for i in 1:num_candidates], (1,num_candidates))
+    @test mapslices(norm, vp.prefvecs; dims=1) ≈ reshape([1. for i in 1:num_offices], (1,num_offices))
     @test overlap(vp, vp) ≈ 1
 end
