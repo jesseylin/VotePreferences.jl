d66d5eb5626a7318a999f59502e86eb28e1c8f5d
Jesse Y Lin <jesseyounglin@gmail.com>
2022-05-28 11:40:10 -0400

    Tests for FPP

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,3 +20,55 @@ const num_voters = 6
     @test mapslices(norm, vp.prefvecs; dims=1) ≈ reshape([1. for i in 1:num_offices], (1,num_offices))
     @test overlap(vp, vp) ≈ 1
 end
+
+# tests for electoral system
+@testset "castballot! sanity checks" begin
+	voters = Vector{VotePreference}(undef, num_voters)
+	for i in 1:num_voters
+		prefvecs = [rand() for i in 1:num_candidates, j in 1:num_offices]
+		voters[i] = VotePreference(prefvecs)
+	end
+
+	voter = voters[1]
+	vote = castballot(voter)
+
+	@test typeof(vote) <: Vector{<:Integer}
+	@test only(size(vote)) == num_offices
+	
+	@test isempty(filter(x -> x > num_candidates, vote))
+	@test isempty(filter(x -> x <= 0, vote))
+end
+
+@testset "countvotes! sanity checks" begin
+	votes = [[rand(1:num_candidates) for i in 1:num_offices] for i in 1:num_voters]
+	votes = permutedims([votes[i][j] for i in eachindex(votes), j in eachindex(votes[1])])
+	counts = Array{Int}(undef, num_offices, num_candidates)
+	
+	countvotes!(counts, votes)
+	@test typeof(counts) <: Array{<:Integer}
+	@test size(counts, 1) == num_offices
+	
+	@test size(counts) == (num_offices, num_candidates)
+	@test vec(sum(counts; dims=2)) == [num_voters for i in axes(counts, 1)]
+end
+
+@testset "determinewinner! sanity checks" begin
+	votes = [[rand(1:num_candidates) for i in 1:num_offices] for i in 1:num_voters]
+	votes = permutedims([votes[i][j] for i in eachindex(votes), j in eachindex(votes[1])])
+	counts = Array{Int}(undef, num_offices, num_candidates)
+	countvotes!(counts, votes)
+
+	outcome = Vector{Int}(undef, num_offices)
+	determinewinner!(outcome, counts)
+	@test typeof(outcome) <: Vector{<:Integer}
+	@test only(size(outcome)) == num_offices
+	@test isempty(filter(x -> x > num_candidates, outcome))
+	@test isempty(filter(x -> x <= 0, outcome))
+
+	@test allunique(outcome)
+end
+
+#@testset "election" begin
+#    election = Election(parameterdict)
+#    @test typeof(election) == Election
+#end
